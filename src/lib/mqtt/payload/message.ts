80ce9194e6802c60dcd595de41d6170d1ca0b9c3
Al Reasat Rafio <rafioar96@gmail.com>
2023-10-19 10:46:51 +0600

    :zap: Mqtt topic function updated with new methods

--- src/lib/mqtt/payload/message.ts
+++ src/lib/mqtt/payload/message.ts
@@ -1,7 +1,14 @@
 import type { Topics } from '../types';
-import temperatureStore from '@stores/payload/temperature';
 import altitudeStore from '@stores/payload/altitude';
-import accelerationStore from '@stores/payload/acceleration';
+import airPressureStore from '@stores/payload/air-pressure';
+import temperatureStore from '@stores/payload/temperature';
+import batteryVoltageStore from '@stores/payload/battery-voltage';
+import tiltAngleStore from '@stores/payload/tilt-angle';
+import airSpeedStore from '@stores/payload/air-speed';
+import commandEchoStore from '@stores/payload/command-echo';
+import gpsCoordinatesStore from '@stores/payload/gps-coordinates';
+import longitudeStore from '@stores/payload/longitude';
+import satellitesTrackedStore from '@stores/payload/satellites-tracked';
 import gyroscopeStore from '@stores/payload/gyroscope';
 interface ITempMessage {
   message: string;
@@ -15,6 +22,79 @@ const getCurrentTime = () => {
   return time;
 };
 
+const altitude = ({ message }: ITempMessage) => {
+  try {
+    const { value } = JSON.parse(message);
+    altitudeStore.update({ value, time: getCurrentTime() });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const airPressure = ({ message }: ITempMessage) => {
+  try {
+    const { value } = JSON.parse(message);
+    airPressureStore.update({ value, time: getCurrentTime() });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const batteryVoltage = ({ message }: ITempMessage) => {
+  try {
+    const { value } = JSON.parse(message);
+    batteryVoltageStore.update({ value, time: getCurrentTime() });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const tiltAngle = ({ message }: ITempMessage) => {
+  try {
+    const { value } = JSON.parse(message);
+    tiltAngleStore.update({ value, time: getCurrentTime() });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const airSpeed = ({ message }: ITempMessage) => {
+  try {
+    const { value } = JSON.parse(message);
+    airSpeedStore.update({ value, time: getCurrentTime() });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const commandEcho = ({ message }: ITempMessage) => {
+  try {
+    const { value } = JSON.parse(message);
+    commandEchoStore.update({ value, time: getCurrentTime() });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const gpsCoordinates = ({ message }: ITempMessage) => {
+  try {
+    const { value } = JSON.parse(message);
+    gpsCoordinatesStore.update({ value, time: getCurrentTime() });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const longitude = ({ message }: ITempMessage) => {
+  try {
+    const { value } = JSON.parse(message);
+    longitudeStore.update({ value, time: getCurrentTime() });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const satellitesTracked = ({ message }: ITempMessage) => {
+  try {
+    const { value } = JSON.parse(message);
+    satellitesTrackedStore.update({ value, time: getCurrentTime() });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const temperature = ({ message }: ITempMessage) => {
   try {
     const { value } = JSON.parse(message);
@@ -25,23 +105,6 @@ const temperature = ({ message }: ITempMessage) => {
   }
 };
 
-const altitude = ({ message }: ITempMessage) => {
-  try {
-    const { value } = JSON.parse(message);
-    altitudeStore.update({ value, time: getCurrentTime() });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const acceleration = ({ message }: ITempMessage) => {
-  try {
-    const { value } = JSON.parse(message);
-    accelerationStore.update({ value, time: getCurrentTime() });
-  } catch (error) {
-    console.log(error);
-  }
-};
 const gyroscope = ({ message }: ITempMessage) => {
   try {
     const { value } = JSON.parse(message);
@@ -53,7 +116,15 @@ const gyroscope = ({ message }: ITempMessage) => {
 
 export const message = {
   temperature: (props: ITempMessage) => temperature(props),
+  airPressure: (props: ITempMessage) => airPressure(props),
+  batteryVoltage: (props: ITempMessage) => batteryVoltage(props),
+  tiltAngle: (props: ITempMessage) => tiltAngle(props),
+  airSpeed: (props: ITempMessage) => airSpeed(props),
+  commandEcho: (props: ITempMessage) => commandEcho(props),
+  gpsCoordinates: (props: ITempMessage) => gpsCoordinates(props),
+  longitude: (props: ITempMessage) => longitude(props),
+  satellitesTracked: (props: ITempMessage) => satellitesTracked(props),
+
   altitude: (props: ITempMessage) => altitude(props),
-  acceleration: (props: ITempMessage) => acceleration(props),
   gyroscope: (props: ITempMessage) => gyroscope(props)
 };
