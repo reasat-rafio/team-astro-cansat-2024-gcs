7344aec0789cfc8995a44947968cdd8ee7eb550f
reasat-rafio <rafioar96@gmail.com>
2023-12-07 19:27:10 +0600

    :hammer: Navbar adjustment

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -13,15 +13,13 @@
 
 <div bind:clientHeight={$navbarHeight}>
   <AppBar>
-    <svelte:fragment slot="lead">Cansat 2024</svelte:fragment>
-    <svelte:fragment slot="trail">
+    <svelte:fragment slot="lead">
       <ul class="flex space-x-2">
         {#each navItems as { icon, name, url }}
           <li>
             <a
-              class="btn {$page.url.pathname === url
-                ? 'variant-ghost'
-                : 'hover:variant-ghost'}"
+              class="btn variant-ghost hover:variant-filled-secondary {$page.url
+                .pathname === url && '!variant-filled-secondary'}"
               href={url}
             >
               <span>{name}</span>
@@ -30,6 +28,27 @@
           </li>
         {/each}
       </ul>
+    </svelte:fragment>
+    <svelte:fragment slot="trail">
+      <button
+        class="btn variant-outline-secondary hover:variant-filled-secondary"
+        ><span>Export CSV</span>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-5 h-5"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+          />
+        </svg>
+      </button>
     </svelte:fragment>
   </AppBar>
 </div>
--- src/components/pages/home/streams/Streams.svelte
+++ src/components/pages/home/streams/Streams.svelte
@@ -10,6 +10,16 @@
     '123456, 10:00:00, 50, Launch, Inflight, 500000, True, False, True, 20°C, 12V, 9:59:59, 498000, 37.7749°N, 122.4194°W, 8, 0.5, -0.5, Success.',
     '123456, 10:00:00, 50, Launch, Inflight, 500000, True, False, True, 20°C, 12V, 9:59:59, 498000, 37.7749°N, 122.4194°W, 8, 0.5, -0.5, Success.',
     '123456, 10:00:00, 50, Launch, Inflight, 500000, True, False, True, 20°C, 12V, 9:59:59, 498000, 37.7749°N, 122.4194°W, 8, 0.5, -0.5, Success.',
+    '123456, 10:00:00, 50, Launch, Inflight, 500000, True, False, True, 20°C, 12V, 9:59:59, 498000, 37.7749°N, 122.4194°W, 8, 0.5, -0.5, Success.',
+    '123456, 10:00:00, 50, Launch, Inflight, 500000, True, False, True, 20°C, 12V, 9:59:59, 498000, 37.7749°N, 122.4194°W, 8, 0.5, -0.5, Success.',
+    '123456, 10:00:00, 50, Launch, Inflight, 500000, True, False, True, 20°C, 12V, 9:59:59, 498000, 37.7749°N, 122.4194°W, 8, 0.5, -0.5, Success.',
+    '123456, 10:00:00, 50, Launch, Inflight, 500000, True, False, True, 20°C, 12V, 9:59:59, 498000, 37.7749°N, 122.4194°W, 8, 0.5, -0.5, Success.',
+    '123456, 10:00:00, 50, Launch, Inflight, 500000, True, False, True, 20°C, 12V, 9:59:59, 498000, 37.7749°N, 122.4194°W, 8, 0.5, -0.5, Success.',
+    '123456, 10:00:00, 50, Launch, Inflight, 500000, True, False, True, 20°C, 12V, 9:59:59, 498000, 37.7749°N, 122.4194°W, 8, 0.5, -0.5, Success.',
+    '123456, 10:00:00, 50, Launch, Inflight, 500000, True, False, True, 20°C, 12V, 9:59:59, 498000, 37.7749°N, 122.4194°W, 8, 0.5, -0.5, Success.',
+    '123456, 10:00:00, 50, Launch, Inflight, 500000, True, False, True, 20°C, 12V, 9:59:59, 498000, 37.7749°N, 122.4194°W, 8, 0.5, -0.5, Success.',
+    '123456, 10:00:00, 50, Launch, Inflight, 500000, True, False, True, 20°C, 12V, 9:59:59, 498000, 37.7749°N, 122.4194°W, 8, 0.5, -0.5, Success.',
+    '123456, 10:00:00, 50, Launch, Inflight, 500000, True, False, True, 20°C, 12V, 9:59:59, 498000, 37.7749°N, 122.4194°W, 8, 0.5, -0.5, Success.',
     '123456, 10:00:00, 50, Launch, Inflight, 500000, True, False, True, 20°C, 12V, 9:59:59, 498000, 37.7749°N, 122.4194°W, 8, 0.5, -0.5, Success.'
   ];
 </script>
@@ -19,7 +29,7 @@
 
   <div class="flex flex-col gap-y-4">
     {#each data as d, index}
-      <p>{index + 1}. {d}</p>
+      <p><span class="text-secondary-500">{index + 1}.</span> {d}</p>
     {/each}
   </div>
 </section>
