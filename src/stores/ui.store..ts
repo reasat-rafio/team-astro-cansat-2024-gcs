960dd1935dad25946ad742bf8caa65f7522b3a37
reasat-rafio <rafioar96@gmail.com>
2024-04-28 14:41:31 +0600

    ui store tweak

--- src/stores/ui.store..ts
+++ src/stores/ui.store..ts
@@ -1,29 +1,15 @@
-import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
-
-type Theme = 'light' | 'dark';
 
 interface UIStore {
   navbarHeight: number;
-  theme: Theme;
   showNotification: boolean;
 }
-
-const initialTheme = browser
-  ? (localStorage.getItem('theme') as Theme) ?? 'light'
-  : 'light';
 
 function createUiStore() {
   const { subscribe, update, set } = writable<UIStore>({
     navbarHeight: 0,
-    theme: initialTheme,
     showNotification: true,
   });
-
-  function setTheme(theme: Theme) {
-    localStorage.setItem('theme', theme);
-    update((state) => ({ ...state, theme }));
-  }
 
   function setNavbarHeight(height: number) {
     update((state) => ({ ...state, navbarHeight: height }));
@@ -33,7 +19,6 @@ function createUiStore() {
     set,
     update,
     subscribe,
-    setTheme,
     setNavbarHeight,
   };
 }
