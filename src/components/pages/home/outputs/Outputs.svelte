6c41f0aebe43892266350c6b5edba6c3e3d9949a
reasat-rafio <rafioar96@gmail.com>
2024-05-02 14:10:14 +0600

    output adjustment

--- src/components/pages/home/outputs/Outputs.svelte
+++ src/components/pages/home/outputs/Outputs.svelte
@@ -1,52 +1,58 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
   import Header from '../Header.svelte';
   import Item from './Item.svelte';
   import { Zap } from 'lucide-svelte';
   import { ScrollArea } from '@/components/ui/scroll-area/index.js';
 
-  let _teamId = '';
-  let missionTime = '';
-  let hsDeployed = false;
-  let pcDeployed = false;
-  let packetCount = '';
-  let activeMode = '';
-  let activeState = '';
-  let latestAltitude = '';
-  let temperature = '';
-  let latestTilt = { x: '', y: '' };
-  let latestGps = {
-    time: '',
-    altitude: '',
-    latitude: '',
-    longitude: '',
-    // sats: '',
-  };
-
-  onMount(() => {});
+  let TEAM_ID = '2043';
+  let MISSION_TIME = '';
+  let PACKET_COUNT = '0';
+  let ACTIVE_MODE = 'GROUND';
+  let ACTIVE_STATE = '';
+  let ALTITUDE = '';
+  let AIR_SPEED = '';
+  let HS_DEPLOYED = 'True';
+  let PC_DEPLOYED = 'True';
+  let TEMPERATURE = '';
+  let VOLTAGE = '12V';
+  let PRESSURE = '132';
+  let GPS_TIME = '12:00:00';
+  let GPS_ALTITUDE = '0';
+  let GPS_LATITUDE = '0';
+  let GPS_LONGITUDE = '0';
+  let GPS_SATS = '0';
+  let TILT_X = '0';
+  let TILT_Y = '0';
+  let TILT_Z = '0';
+  let ROT_Z = '0';
+  let CMD_ECHO = '0';
 </script>
 
 <ScrollArea class="h-full w-full p-4">
   <Header icon={Zap} title="Outputs" />
 
   <div class="mt-5 flex flex-col gap-y-4">
-    <Item formatKey="Team Id" value={_teamId} />
-    <Item formatKey="Mission Time" value={missionTime} />
-    <Item formatKey="Packet Count" value={packetCount} />
-    <Item formatKey="Mode" value={activeMode} />
-    <Item formatKey="State" value={activeState} />
-    <Item formatKey="Altitude" value={latestAltitude} />
-    <Item formatKey="Hs Deployed" value={String(hsDeployed)} />
-    <Item formatKey="Pc Deployed" value={String(pcDeployed)} />
-    <Item formatKey="Mast Raised" value="True" />
-    <Item formatKey="Temperature" value={temperature} />
-    <Item formatKey="Voltage" value="12V" />
-    <Item formatKey="Gps Time" value={latestGps.time} />
-    <Item formatKey="Gps Altitude" value={latestGps.altitude} />
-    <Item formatKey="Gps Latitude" value={latestGps.latitude} />
-    <Item formatKey="Gps Longitude" value={latestGps.longitude} />
-    <Item formatKey="Gps Sats" value="8" />
-    <Item formatKey="Tilt X" value={latestTilt.x} />
-    <Item formatKey="Tilt Y" value={latestTilt.y} />
+    <Item formatKey="TEAM_ID" value={TEAM_ID} />
+    <Item formatKey="MISSION_TIME" value={MISSION_TIME} />
+    <Item formatKey="PACKET_COUNT" value={PACKET_COUNT} />
+    <Item formatKey="MODE" value={ACTIVE_MODE} />
+    <Item formatKey="STATE" value={ACTIVE_STATE} />
+    <Item formatKey="ALTITUDE" value={ALTITUDE} />
+    <Item formatKey="AIR_SPEED" value={String(AIR_SPEED)} />
+    <Item formatKey="HS_DEPLOYED" value={String(HS_DEPLOYED)} />
+    <Item formatKey="PC_DEPLOYED" value={PC_DEPLOYED} />
+    <Item formatKey="TEMPERATURE" value={TEMPERATURE} />
+    <Item formatKey="VOLTAGE" value={VOLTAGE} />
+    <Item formatKey="PRESSURE" value={PRESSURE} />
+    <Item formatKey="GPS_TIME" value={GPS_TIME} />
+    <Item formatKey="GPS_ALTITUDE" value={GPS_ALTITUDE} />
+    <Item formatKey="GPS_LATITUDE" value={GPS_LATITUDE} />
+    <Item formatKey="GPS_LONGITUDE" value={GPS_LONGITUDE} />
+    <Item formatKey="GPS_SATS" value={GPS_SATS} />
+    <Item formatKey="TILT_X" value={TILT_X} />
+    <Item formatKey="TILT_Y" value={TILT_Y} />
+    <Item formatKey="TILT_Z" value={TILT_Z} />
+    <Item formatKey="ROT_Z" value={ROT_Z} />
+    <Item formatKey="CMD_ECHO" value={CMD_ECHO} />
   </div>
 </ScrollArea>
