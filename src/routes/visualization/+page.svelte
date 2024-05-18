e14861811245c7f90794a507377aae103b6a59e6
reasat-rafio <rafioar96@gmail.com>
2024-05-18 14:45:35 +0600

    ui store updated

--- src/routes/visualization/+page.svelte
+++ src/routes/visualization/+page.svelte
@@ -1,21 +1,19 @@
 <script lang="ts">
   import Charts from '@/components/charts/Charts.svelte';
-  import { uiStore } from '@/stores/ui.store';
-  import VirtualList from 'svelte-tiny-virtual-list';
+  // import { uiStore } from '@/stores/ui.store';
+  // import VirtualList from 'svelte-tiny-virtual-list';
 </script>
 
-<VirtualList
+<!-- <VirtualList
   height="100%"
   width="100%"
   scrollDirection="vertical"
   itemCount={1}
-  itemSize={850}>
-  <section
-    slot="item"
-    class="h-full w-full [&>*]:border-gray-600 [&>*]:p-5"
-    style="height: calc(100vh - {$uiStore.navbarHeight}px);">
-    <div class="h-screen divide-y divide-gray-500">
-      <Charts class="pt-0" />
-    </div>
-  </section>
-</VirtualList>
+  itemSize={850}> -->
+<!-- slot="item" -->
+<section class="h-full w-full [&>*]:border-gray-600 [&>*]:p-5">
+  <div class="min-h-screen divide-y divide-gray-500">
+    <Charts class="pt-0" />
+  </div>
+</section>
+<!-- </VirtualList> -->
